942608251b01d25d1f16fb278e267f91ad76e2e8
인성 <me@insd.dev>
2024-07-21 14:10:15 +0900

    프로젝트 구조 변경 (#1)
    - 기반 라이브러리를 discordx로 변경
    - 프로젝트 폴더 구조를 discordx 보일러플레이트와 동일하게 변경
    - @discordx/importer 라이브러리를 이용하여 커맨드 파일 자동 임포트 구현
    - 패키지 관리자로 yarn 사용 (강제는 절대 아닙니다!)

---
+++ src/main.ts
@@ -0,0 +1,66 @@
+import { dirname, importx } from "@discordx/importer";
+import type { Interaction, Message } from "discord.js";
+import { IntentsBitField } from "discord.js";
+import { Client } from "discordx";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const bot = new Client({
+  // to use only guild commands
+  botGuilds: [(client) => client.guilds.cache.map((guild) => guild.id)],
+
+  // Discord intents
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.GuildMessageReactions,
+    IntentsBitField.Flags.GuildVoiceStates,
+    IntentsBitField.Flags.MessageContent,
+  ],
+
+  // Debug logs are disabled in silent mode
+  silent: false,
+});
+
+bot.once("ready", async () => {
+  // Make sure all guilds are cached
+  await bot.guilds.fetch();
+
+  // Synchronize applications commands with Discord
+  void await bot.initApplicationCommands();
+
+  // To clear all guild commands, uncomment this line,
+  // This is useful when moving from guild commands to global commands
+  // It must only be executed once
+  //
+  //  await bot.clearApplicationCommands(
+  //    ...bot.guilds.cache.map((g) => g.id)
+  //  );
+
+  console.log("Bot started");
+});
+
+bot.on("interactionCreate", (interaction: Interaction) => {
+  bot.executeInteraction(interaction);
+});
+
+bot.on("messageCreate", (message: Message) => {
+  void bot.executeCommand(message);
+});
+
+async function run() {
+  // Register all commands and events in the ./commands and ./events folders
+  await importx(`${dirname(import.meta.url)}/{events,commands}/**/*.{ts,js}`);
+
+  // Let's start the bot
+  if (!process.env.DISCORD_TOKEN) {
+    throw Error("Could not find DISCORD_TOKEN in your environment");
+  }
+
+  // Log in with your bot token
+  await bot.login(process.env.DISCORD_TOKEN);
+}
+
+void run();
